f7612515f27b93d2cae955453f2196d02464aea3
triplelog <robmwilc@gmail.com>
2020-03-28 13:11:05 -0400

    First

--- static/js/game.js
+++ static/js/game.js
@@ -1,8 +1,14 @@
+var selectedButton = 0;
 var el = document.getElementById('buttonRow');
 var buttons = el.querySelectorAll('span');
 for (var i=0;i<buttons.length;i++){
 	buttons[i].addEventListener('click',chgButton);
 }
 function chgButton(evt) {
-	alert(evt.target.id);
+	var el = evt.target;
+	while (el && el.tagName && el.tagName != 'span'){
+		el = el.parentElement;
+	}
+	selectedButton = parseInt(el.id.split('-')[1]);
+	alert(selectedButton);
 }
