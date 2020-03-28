2382a7f7207a90ae0a50c726dec08086a892ce40
triplelog <robmwilc@gmail.com>
2020-03-28 13:19:46 -0400

    First

--- static/js/game.js
+++ static/js/game.js
@@ -9,6 +9,20 @@ function chgButton(evt) {
 	while (el && el.tagName && el.tagName != 'SPAN'){
 		el = el.parentElement;
 	}
-	selectedButton = parseInt(el.id.split('-')[1]);
+	if (selectedButton != parseInt(el.id.split('-')[1])){
+		selectedButton = parseInt(el.id.split('-')[1]);
+		el.classList.add('selected');
+	}
+	else {
+		selectedButton = 0;
+		el.classList.remove('selected');
+	}
+	var ell = document.getElementById('buttonRow');
+	var buttons = ell.querySelectorAll('span');
+	for (var i=0;i<buttons.length;i++){
+		if (buttons[i].id != el.id){
+			buttons[i].classList.remove('selected');
+		}
+	}
 	alert(selectedButton);
 }
