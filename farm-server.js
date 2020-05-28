ad77702fa731a944cb7b9ec5a79bad483cefce45
triplelog <robmwilc@gmail.com>
2020-05-28 14:59:33 -0400

    First

--- farm-server.js
+++ farm-server.js
@@ -105,7 +105,7 @@ for (var ii=0;ii<4;ii++ ){
 	if (puzzleTypes[ii]=='daily'){
 		data = fs.readFileSync('./games/hardDaily.txt', 'utf8');
 		lines = data.split('\n');
-		lines.splice(0,110);
+		lines = lines.slice(0,110);
 		console.log(lines.length);
 		data = fs.readFileSync('./games/easyDaily.txt', 'utf8');
 		lines = lines.concat(data.split('\n'));
